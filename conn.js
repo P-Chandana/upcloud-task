0a9782faa6199ba600d590f465adfabc639fd68f
Chandana <52034745+P-Chandana@users.noreply.github.com>
2021-04-30 18:54:38 +0530

    Delete conn.js

--- conn.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/userdb", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-}).then(()=>{
-    console.log("connection successful");
-}).catch((e)=>{
-    console.log(e);
-})
