40d3feec48ec411d9aa3c32b19098765f55fe4d5
Chandana <52034745+P-Chandana@users.noreply.github.com>
2021-04-30 18:55:48 +0530

    Add files via upload

---
+++ api/conn.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/userdb", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+}).then(()=>{
+    console.log("connection successful");
+}).catch((e)=>{
+    console.log(e);
+})
